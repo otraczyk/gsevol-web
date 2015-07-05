315b618f0a32ee04267565093bf18a176120c9a1
Olga Traczyk <o.traczyk@student.uw.edu.pl>
2015-07-05 13:01:23 +0200

    Extracting SVG source

--- api/bindings.py
+++ api/bindings.py
@@ -7,19 +7,19 @@ from django.conf import settings
 
 
 def launch(params, timeout=300):
-    """
-    Basic implementation: gsevol is launched in a subprocess, 
+    """Launch GSevol subprocess.
+
+    Basic implementation: gsevol is launched in a subprocess,
     given a time limit (default 300 seconds).
 
     :param params: a list of parameters which will be passed by the
         command line. Includes input data, flags and special '&'-commands
     :param timeout: time limit for the process in seconds
 
-    If the process ends successfully, returns the output file handle 
-    from stdout.
+    If the process ends successfully, returns the output from stdout.
     """
     gse_process = subprocess.Popen(
-        settings.GSEVOL_COMMAND.split(' ') + params, 
+        settings.GSEVOL_COMMAND.split(' ') + params,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     timer = time.time() + timeout
@@ -27,17 +27,27 @@ def launch(params, timeout=300):
         if time.time() > timer:
             gse_process.terminate()
             raise RuntimeError("Gsevol process timeout", params)
-    if gse_process.returncode != 0:
-        out, err = gse_process.communicate()
-        raise RuntimeError("Gsevol error", err, params)
+    out, err = gse_process.communicate()
+    if err:
+        raise RuntimeError("Gsevol error", err, params, out)
+    return out
 
-    return gse_process.stdout
-
+def launch_command(command):
+    """Launches Gsevol with given command string."""
+    out = launch(command.split(' '))
+    return out
 
 def gen_randbin_f():
     """
     Demo: generates random binary gene and species trees with leaves a-f.
     """
     command = '-g &randbin(a-f) -s &randbin(a-f) -egsG'
-    out = launch(command.split(' '))
-    return out.read()
+    return launch_command(command)
+
+def draw_randbin_s():
+    """
+    Demo: return random tree svg.
+    """
+    return launch_command(
+        '-g &randbin(a-f) -s &randbin(a-f) -dgS -C outputfile="/dev/stdout"'
+        )
