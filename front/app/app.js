7d9e52c6215e1ad5d7c5706d0f030bbf0b100419
Olga Traczyk <o.traczyk@student.uw.edu.pl>
2015-07-15 11:27:13 +0200

    Simple ajaxError handling

--- front/app/app.js
+++ front/app/app.js
@@ -14,6 +14,10 @@ renderResults = function(data){
     $('#results').html(data.svg);
 }
 
+$(document).ajaxError(function() {
+  $('#alerts').append('<li class="danger alert">Error processing input</li>');
+});
+
 $(function(){
     $('#input-form').submit(sendTrees);
 })
