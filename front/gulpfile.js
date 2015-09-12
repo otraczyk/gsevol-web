e2d962085535657b89b17fe9b499c5fa5aa35bfa
Olga Traczyk <o.traczyk@student.uw.edu.pl>
2015-09-12 15:53:40 +0200

    Setup wiredep

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -4,16 +4,19 @@ var gulp  = require('gulp-help')(require('gulp')),
     watch = require('gulp-watch');
 
 var react = require('gulp-react');
+var wiredep = require('wiredep').stream;
 
 gulp.task(
     'watch',
     'Watch for any changes in js, scss, html files',
-    ['watch-react'],
+    ['watch-react', 'watch-deps'],
     function(){}
 );
 
 gulp.task('build-react', false, buildReact);
 gulp.task('watch-react', ['build-react'], watchReact);
+gulp.task('watch-deps', watchDeps);
+gulp.task('inject', injectDeps);
 
 function buildReact(){
     return gulp.src('./app/*.jsx')
@@ -22,5 +25,33 @@ function buildReact(){
 }
 
 function watchReact(){
-    return watch('./app/*.jsx', buildReact);
+    return watch('./app/*.jsx', function(){
+        gulp.start('build-react');
+    });
+}
+
+function watchDeps(){
+    return watch('./bower_components/**/*.*', function(){
+        gulp.start('inject');
+    });
+}
+
+function injectDeps(){
+    console.log("Injecting bower dependencies");
+    return gulp.src('templates/base.html')
+        .pipe(wiredep(
+        {
+            directory: 'bower_components/',
+            exclude: ['bower_components/gumby/*'],
+            ignorePath: '../bower_components/',
+            fileTypes: {
+                html: {
+                  replace: {
+                    js: '<script src="{% static \'{{filePath}}\' %}"></script>',
+                    css: '<link rel="stylesheet" href="{% static \'{{filePath}}\' %}" />'
+                  }
+                }
+              }
+        }))
+    .pipe(gulp.dest('templates/'));
 }
