a4b1141c188de86c7c4c9f990eafcb5a464aa665
Olga Traczyk <o.traczyk@student.uw.edu.pl>
2016-08-31 15:05:56 +0200

    Provisioning - part 1

--- settings.py
+++ settings.py
@@ -87,7 +87,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR, 'collectedstatic')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     'front/dist/',
